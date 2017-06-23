90a74cfa0c1f0b7ff5728de0e71ab40eda44746d
Michael Rod <michaelrod999@gmail.com>
2017-06-23 11:47:40 -0600

    I added the program logic

--- Sandbox/src/sandbox/Sandbox.java
+++ Sandbox/src/sandbox/Sandbox.java
@@ -5,6 +5,8 @@
  */
 package sandbox;
 
+import java.util.Scanner;
+
 /**
  * This is a test of Git and Github
  * @author Michael
@@ -14,8 +16,36 @@ public class Sandbox {   // this is a test of the editor pulling down
     /**
      * @param args the command line arguments
      */
+    
+    static final int MAX_NUM = 200;
+    static int counts [] = new int [MAX_NUM];
+    static int number_given;
+    static Scanner input = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Prompt for input
+        System.out.print("Enter the integers between 1 and " + MAX_NUM + " (end input with 0): ");
+        
+        do { 
+            number_given = input.nextInt();
+            counts[number_given]++;
+            
+        } while (number_given != 0);
+        
+        output(counts);
+        
+        
+    }
+    
+    
+    public static void output (int [] x){
+        for (int counter = 1; counter < MAX_NUM; counter++)
+            if (x[counter] > 1)
+                System.out.println(counter + " occurs " + x[counter] + " times");
+            else if (x[counter] == 1)
+                System.out.println(counter + " occurs " + x[counter] + " time");
+        
     }
     
 }
+
