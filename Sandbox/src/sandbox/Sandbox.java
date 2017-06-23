7e6a1a05c73d321c272040eca698a82af6e14a98
obrien2112 <michaelrod999@gmail.com>
2017-06-23 12:09:20 -0600

    Update Sandbox.java

--- Sandbox/src/sandbox/Sandbox.java
+++ Sandbox/src/sandbox/Sandbox.java
@@ -8,7 +8,7 @@ package sandbox;
 import java.util.Scanner;
 
 /**
- * This is a test of Git and Github  #2
+ * This is a test of Git and Github  #2 This small edit should pull down
  * @author Michael
  */
 public class Sandbox {   // this is a test of the editor pulling down
